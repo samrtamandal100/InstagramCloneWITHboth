35692272432946aac5b86e721f15c70aca33d1ec
samratmandal100 <quicktech08967@gmail.com>
2023-06-30 14:56:43 -0400

    push instagram

---
+++ backend/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoUrl: "mongodb://127.0.0.1:27017/instagram",
+    Jwt_secret: "faslkfocvneofu"
+
+};
+
+//mongodb+srv://FlyingPanda730:UgpngUX6DVAx1Xq0@cluster0.ormku05.mongodb.net/?retryWrites=true&w=majority
